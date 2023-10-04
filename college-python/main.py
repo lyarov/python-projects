d8b0e338aa16e10b92d020f5af7d38a46978fc29
Фёдоров Илья <dyudyafed006@gmail.com>
2023-10-04 19:17:48 +0600

    03.10.2023 лаб6

--- college-python/main.py
+++ college-python/main.py
@@ -1,20 +1,3 @@
-n, m = map(int, input().split())
-res1 = []
-res2 = []
-a = []
-
-for i in range(n):
-    t = list(map(int, input().split()))
-    mn = min(t)
-    res1.append(mn)
-    a.append(t)
-
-res1.sort()
-
-for i in range(m):
-    mx = max(a[j][i] for j in range(n))
-    res2.append(mx)
-
-res2.sort()
-
-print(res1[-1], res2[0])
+string = "информатика"
+print(string[2:7])
+print(string[-4:-1])
