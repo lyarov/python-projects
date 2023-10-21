24f19ecab28d07680b386814b1efcf84b7d5295f
Фёдоров Илья <dyudyafed006@gmail.com>
2023-10-21 13:50:39 +0600

    8910

--- college-python/main.py
+++ college-python/main.py
@@ -1,81 +1,24 @@
-import turtle
+from tkinter import *
+from tkinter import ttk
 
-t2 = turtle.Turtle()
-t2.color("white")
-t2.goto(0, -200)
-t2.color("black")
-def prepare_fig(fig, x, y):
-    fig.hideturtle()
-    fig.penup()
-    fig.setposition(x, y)
-    fig.speed(13)
+clicks = 0
 
-def draw_square(fig, color, side_length):
-    fig.pendown()
-    fig.fillcolor(color)
-    fig.begin_fill()
-    for i in range(4):
-        fig.fd(side_length)
-        fig.rt(90)
-    fig.end_fill()
+root = Tk()
 
-def message(text, color):
-    circ.hideturtle()
-    circ.goto(0, 0)
-    circ.color(color)
-    sq.clear()
-    sq2.clear()
-    print(moves)
-    circ.write(text, font=("Arial", 12, "bold"))
+root.title("Gui python")
 
-def win_or_die(moves):
-    if -20 < circ.xcor() < 40 and 10 < circ.ycor() < 70:
-        message(GAME_OVER_MSG + str(moves), 'red')
-    if -60 < circ.xcor() < -20 and 50 < circ.ycor() < 90:
-        message(WIN_MSG + str(moves), 'green')
+root.geometry("300x400")
 
-def movey(deltay):
-    global moves
-    y = circ.ycor() + deltay
-    circ.sety(y)
-    moves += 1
-    t2.clear()
-    t2.write(moves, font=("Arial", 12, "bold"))
-    win_or_die(moves)
+def btnClick():
+    global clicks
+    clicks += 1
+    btn["text"] = f"Clicks {clicks}"
 
-def movex(deltax):
-    global moves
-    x = circ.xcor() + deltax
-    circ.setx(x)
-    moves += 1
-    t2.clear()
-    t2.write(moves, font=("Arial", 12, "bold"))
-    win_or_die(moves)
 
-wndow = turtle.Screen()
-wndow.title("Circle game")
-wndow.setup(500, 500)
+btn = ttk.Button(text="But", command=btnClick)
 
-circ = turtle.Turtle()
-circ.penup()
-circ.shape("circle")
-circ.color("orange")
 
-sq = turtle.Turtle()
-prepare_fig(sq, -20, 70)
-draw_square(sq, 'red', 60)
-sq2 = turtle.Turtle()
-prepare_fig(sq2, -60, 90)
-draw_square(sq2, 'green', 40)
+btn.pack()
 
-moves = 0
-GAME_OVER_MSG = 'Game over!\nСделано шагов: '
-WIN_MSG = 'Победа!\nСделано шагов: '
-STEP = 10
 
-turtle.listen()
-turtle.onkeypress(lambda: movey(STEP), 'Up')
-turtle.onkeypress(lambda: movey(-STEP), 'Down')
-turtle.onkeypress(lambda: movex(STEP), 'Right')
-turtle.onkeypress(lambda: movex(-STEP), 'Left')
-turtle.done()
+root.mainloop()
